39fcd6fdd8ba60111848cfbb8c68ee7ed376a29f
Thomas Schell <thom.schell@gmail.com>
2015-08-29 02:25:30 +0200

    started to implement streams for keyboard navigation

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -5,8 +5,11 @@ import Home from './home.jsx'
 import Streams from './streams.js'
 import Bacon from 'baconjs'
 
+var streams = new Streams();
+
 const appState = Bacon.combineTemplate({
-    date: Streams.Timer
+    date: streams.Timer.toProperty(),
+    currentHomeWidget: streams.CurrentHomeWidget.toProperty()
     // add other streams here
 });
 
--- src/js/streams.js
+++ src/js/streams.js
@@ -1,8 +1,81 @@
 
 import Bacon from 'baconjs'
 
-export default {
-    Timer : Bacon.interval(500).map(function() {
-        return new Date();
-    })
+export default class Streams {
+    constructor() {
+        // Used to build other streams
+        this.Input = Bacon.fromEvent(window.document.body, "keyup").filter(e => {
+            return e.keyCode >= 37 && e.keyCode <=40;
+        }).map(e => {
+            var direction;
+            switch(e.keyCode) {
+                case 37: direction = 'left'; break;
+                case 38: direction = 'top'; break;
+                case 39: direction = 'right'; break;
+                case 40: direction = 'down'; break;
+            }
+            return direction;
+        })//.log();
+
+        this.Timer = Bacon.interval(500).map(() => {
+            return new Date();
+        })//.log();
+
+        this.CurrentHomeWidget = this.Input.scan(0, (currentVal, direction) => {
+            var newVal = currentVal;
+            switch(currentVal) {
+                case 0: //logo
+                    switch(direction) {
+                        case 'bottom':  newVal = 0; break;
+                        case 'top':     newVal = 0; break;
+                        case 'left':    newVal = 0; break;
+                        case 'right':   newVal = 0; break;
+                    }
+                    break;
+                case 1: // meteo
+                    switch(direction) {
+                        case 'bottom':  newVal = 0; break;
+                        case 'top':     newVal = 0; break;
+                        case 'left':    newVal = 0; break;
+                        case 'right':   newVal = 0; break;
+                    }
+                    break;
+                case 2: // time
+                    switch(direction) {
+                        case 'bottom':  newVal = 0; break;
+                        case 'top':     newVal = 0; break;
+                        case 'left':    newVal = 0; break;
+                        case 'right':   newVal = 0; break;
+                    }
+                    break;
+                case 3: // nav
+                    switch(direction) {
+                        case 'bottom':  newVal = 0; break;
+                        case 'top':     newVal = 0; break;
+                        case 'left':    newVal = 0; break;
+                        case 'right':   newVal = 0; break;
+                    }
+                    break;
+                case 4: // messages
+                    switch(direction) {
+                        case 'bottom':  newVal = 0; break;
+                        case 'top':     newVal = 0; break;
+                        case 'left':    newVal = 0; break;
+                        case 'right':   newVal = 0; break;
+                    }
+                    break;
+                case 5: // reminders
+                    switch(direction) {
+                        case 'bottom':  newVal = 0; break;
+                        case 'top':     newVal = 0; break;
+                        case 'left':    newVal = 0; break;
+                        case 'right':   newVal = 0; break;
+                    }
+                    break;
+
+            }
+            return newVal;
+        }).log();
+    }
 }
+
