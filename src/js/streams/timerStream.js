00bd31a8c89a3bb16b9b1462dcafd40805f28233
Thomas Schell <thom.schell@gmail.com>
2015-12-12 01:58:46 +0100

    use cold observable

--- src/js/streams/timerStream.js
+++ src/js/streams/timerStream.js
@@ -3,7 +3,7 @@ import Rx from 'rx'
 
 export default class TimerStream {
     constructor() {
-        return Rx.Observable.timer(0, 300).map(time => new Date())
+        return Rx.Observable.timer(0, 300).map(time => new Date()).publish().refCount();
     }
 }
 
