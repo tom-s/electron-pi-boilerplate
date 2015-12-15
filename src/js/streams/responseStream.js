b85827a8064b9294a6de22fd64344348adadbd66
Thomas Schell <thom.schell@gmail.com>
2015-12-15 22:13:50 +0100

    fix double ajax call bug

--- src/js/streams/responseStream.js
+++ src/js/streams/responseStream.js
@@ -14,7 +14,7 @@ export default class ResponseStream {
         var speechStream = SpeechToTextStream.stream;
 
         // just added the scan, check if it works
-        return speechStream.filter(this._filter).flatMapLatest(this._fetchResponse).flatMapLatest(this._handleResponse.bind(this)).map((res) => {
+        return speechStream.filter(this._filter).distinct((data) => { return data.result.final; }).flatMapLatest(this._fetchResponse).flatMapLatest(this._handleResponse.bind(this)).map((res) => {
             console.log("RES", res);
             return res;
         }).publish().refCount();
